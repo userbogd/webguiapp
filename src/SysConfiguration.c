031d647d9cd09ae69b41999596261bf14eb5802f
Bogdan Pilyugin <bogd@live.ru>
2023-08-21 15:38:32 +0200

    ethernet dhcp default value set

--- src/SysConfiguration.c
+++ src/SysConfiguration.c
@@ -291,6 +291,8 @@ static void ResetSysConfig(SYS_CONFIG *Conf)
            sizeof(CONFIG_WEBGUIAPP_WIFI_SSID_STA));
     memcpy(Conf->wifiSettings.InfSecurityKey, CONFIG_WEBGUIAPP_WIFI_KEY_STA,
            sizeof(CONFIG_WEBGUIAPP_WIFI_KEY_STA));
+
+    Conf->wifiSettings.Flags1.bIsDHCPEnabled = false;
 #if CONFIG_WEBGUIAPP_WIFI_DHCP_ON
     Conf->wifiSettings.Flags1.bIsDHCPEnabled = true;
 #endif
@@ -308,7 +310,13 @@ Conf->ethSettings.Flags1.bIsETHEnabled = CONFIG_WEBGUIAPP_ETHERNET_ON;
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_ETH_IP_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.IPAddr);
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_ETH_MASK_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.Mask);
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_ETH_GATEWAY_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.Gateway);
-Conf->ethSettings.Flags1.bIsDHCPEnabled = CONFIG_WEBGUIAPP_ETHERNET_DHCP_DEFAULT;
+
+Conf->ethSettings.Flags1.bIsDHCPEnabled = false;
+#if CONFIG_WEBGUIAPP_ETHERNET_DHCP_DEFAULT
+Conf->ethSettings.Flags1.bIsDHCPEnabled = true;
+#endif
+
+
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_DNS1_ADDRESS_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.DNSAddr1);
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_DNS2_ADDRESS_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.DNSAddr2);
 esp_netif_str_to_ip4(CONFIG_WEBGUIAPP_DNS3_ADDRESS_DEFAULT, (esp_ip4_addr_t*) &Conf->ethSettings.DNSAddr3);
